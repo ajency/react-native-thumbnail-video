b8676ebdf882e91af5ac6bf8df33d3927747e5de
lateshK <42994943+lateshK@users.noreply.github.com>
2020-03-23 15:43:19 +0530

    Update youtube.js

--- src/helpers/youtube.js
+++ src/helpers/youtube.js
@@ -1,6 +1,6 @@
 export const getVideoId = url => {
   const result = url.split(/^.*((youtu.be\/)|(v\/)|(\/u\/\w\/)|(embed\/)|(watch\?))\??v?=?([^#&?]*).*/);
-  const videoIdWithParams = result[2];
+  const videoIdWithParams = result[7];
 
   if (videoIdWithParams !== undefined) {
     const cleanVideoId = videoIdWithParams.split(/[^0-9a-z_-]/i)[0];
